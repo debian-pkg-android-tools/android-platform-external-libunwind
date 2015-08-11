1097b7b9a88d3efa68b5fbef69b3b75e0c8dc298
Kai-Chung Yan <seamlikok@gmail.com>
2015-08-11 19:53:41 +0800

    d/detect_arch.mk: Cover all MIPS and ARM CPUs

--- debian/detect_arch.mk
+++ debian/detect_arch.mk
@@ -9,12 +9,21 @@ endif
 ifeq ($(DEB_HOST_ARCH_CPU), arm)
   CPU = arm
 endif
-ifeq ($(DEB_HOST_ARCH_CPU), armhf)
+ifeq ($(DEB_HOST_ARCH_CPU), armeb)
   CPU = arm
+endif
+ifeq ($(DEB_HOST_ARCH_CPU), arm64)
+  CPU = arm64
 endif
 ifeq ($(DEB_HOST_ARCH_CPU), mips)
   CPU = mips
 endif
 ifeq ($(DEB_HOST_ARCH_CPU), mipsel)
   CPU = mips
+endif
+ifeq ($(DEB_HOST_ARCH_CPU), mips64)
+  CPU = mips64
+endif
+ifeq ($(DEB_HOST_ARCH_CPU), mips64el)
+  CPU = mips64
 endif
